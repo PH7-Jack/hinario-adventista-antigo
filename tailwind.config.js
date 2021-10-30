961fac04e59366c819448aa1993a3020975ea4b6
PH7-Jack <pedrolivertwd@gmail.com>
2021-10-30 00:13:43 -0300

    chore: add wireui preset and others changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,37 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
     mode: 'jit',
+    presets: [
+        require('./vendor/ph7jack/wireui/tailwind.config.js')
+    ],
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './vendor/ph7jack/wireui/resources/**/*.blade.php',
+        './vendor/ph7jack/wireui/ts/**/*.ts',
+        './vendor/ph7jack/wireui/src/View/**/*.php'
     ],
 
     theme: {
         extend: {
+            colors: {
+                blueGray: colors.blueGray
+            },
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            boxShadow: {
+                indigo: '0 4px 6px -1px rgba(129,140,248, 0.4), 0 2px 4px -1px rgba(129,140,248, 0.4)',
+            }
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-};
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
+}
