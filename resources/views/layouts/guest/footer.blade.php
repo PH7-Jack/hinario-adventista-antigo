89ddf9ebd89ce6d51be45c17d6d57055f786ad65
PH7-Jack <pedrolivertwd@gmail.com>
2021-11-20 01:09:04 -0300

    feat: add repository link into the footer

--- resources/views/layouts/guest/footer.blade.php
+++ resources/views/layouts/guest/footer.blade.php
@@ -1,3 +1,5 @@
-<span class="text-xs text-gray-400 text-center mt-auto mb-4">
+<a class="text-xs text-gray-400 text-center mt-auto mb-4"
+    href="https://github.com/PH7-Jack/hymnal-iasd"
+    target="_Blank">
     Open Source Project
-</span>
+</a>
