adb74c0d4cb17444bf1bc76154f8956c0b84626f
PH7-Jack <pedrolivertwd@gmail.com>
2021-11-20 01:36:07 -0300

    style: add breakpoint fontsize

--- resources/views/layouts/guest/footer.blade.php
+++ resources/views/layouts/guest/footer.blade.php
@@ -1,4 +1,4 @@
-<a class="text-xs text-gray-400 text-center mt-auto mb-4"
+<a class="text-xs sm:text-base xl:text-lg text-gray-400 text-center mt-auto mb-4"
     href="https://github.com/PH7-Jack/hymnal-iasd"
     target="_Blank">
     Open Source Project
