ca89d06509882a1ffc43813e845c64b7fe424606
PH7-Jack <pedrolivertwd@gmail.com>
2022-03-12 17:46:07 -0300

    chore: rename project

--- resources/views/layouts/guest/footer.blade.php
+++ resources/views/layouts/guest/footer.blade.php
@@ -1,5 +1,5 @@
 <a class="text-xs sm:text-base xl:text-lg text-gray-400 text-center mt-auto mb-4"
-    href="https://github.com/PH7-Jack/hymnal-iasd"
+    href="https://github.com/PH7-Jack/hinario-adventista"
     target="_Blank">
     Open Source Project
 </a>
