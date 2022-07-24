7ee49475a38089bb6512a1f8b5c07993b5cf6f9a
PH7-Jack <pedrolivertwd@gmail.com>
2022-07-23 23:30:23 -0300

    make vite support ssl

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,5 @@
-import fs from 'fs'
-import { homedir } from 'os'
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import laravel, { refreshPaths } from 'laravel-vite-plugin'
-
-const host = 'hinario.test'
-
-function detectServerConfig(host) {
-    let keyPath = resolve(homedir(), `.valet/Certificates/${host}.key`)
-    let certificatePath = resolve(homedir(), `.valet/Certificates/${host}.crt`)
-
-    if (!fs.existsSync(keyPath)) {
-        return {}
-    }
-
-    if (!fs.existsSync(certificatePath)) {
-        return {}
-    }
-
-    return {
-        hmr: {host},
-        host,
-        https: {
-            key: fs.readFileSync(keyPath),
-            cert: fs.readFileSync(certificatePath),
-        },
-    }
-}
-
+import laravel from 'laravel-vite-plugin'
 import mkcert from'vite-plugin-mkcert'
 
 export default defineConfig({
@@ -39,15 +11,9 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/js/pwa.js',
             ],
-            refresh: [
-                ...refreshPaths,
-                'app/Http/Livewire/**'
-            ]
         }),
     ],
     server: {
         https: true,
-        // host: 'localhost',
    },
-    // server: detectServerConfig(host)
 })
